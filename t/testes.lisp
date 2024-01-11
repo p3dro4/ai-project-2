50549f9f1e7ed4279195327f9e8bb6c15ff2150c
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-01-11 22:58:24 +0000

    Update test strucure and Remove package declaration

--- t/testes.lisp
+++ t/testes.lisp
@@ -2,16 +2,11 @@
 ;;;; Ficheiro que carrrega todos os testes
 ;;;; Autores: 202100230 - Pedro Anjos, 202100225 - André Meseiro
 
-(defpackage :testes
-    (:export verificar-igual verificar-nao-igual)
-    (:use :common-lisp)
-)
-
 ;;; Carregar ficheiros de código
 
 ;; Carrega o caminho para os ficheiros de código
 (defun inicializar-testes ()
-  "Inicializa o programa"
+  "Inicializa os testes"
   (format t ";; A carregar ficheiros...~%")
   (cond ((not (null *load-pathname*)) (progn (carregar-funcao-caminho-raiz *load-pathname*) (format t ";; Ficheiros carregados.~%")))
         ((not (null *compile-file-pathname*)) (progn (carregar-funcao-caminho-raiz *compile-file-pathname*) (format t ";; Ficheiros carregados.~%")))
@@ -30,39 +25,71 @@
 
 ;;; Funções de teste
 
-(defun verificar-igual (valor-esperado valor-real &key (nome-teste "*TESTE-GENERICO*"))
-    (cond ((equal valor-real valor-esperado) (format t "# ~a | -~%" nome-teste))
-          (t (progn (format t "# ~a | X~%" nome-teste)
-                    (format t " =valor esperado: ~a~%" valor-esperado)
-                    (format t " =valor obtido: ~a~%" valor-real)
-             )
-          )
-    )
+;; Função que verifica se o valor esperado é igual ao valor real
+(defun verificar-igual (valor-esperado valor-real &optional (nome-teste "*TESTE-GENERICO*"))
+  "Verifica se o valor esperado é igual ao valor real"
+  (cond ((equal valor-real valor-esperado) (format t "# ~40,1,1,@<'~a'~> | -~%" nome-teste))
+        (t (progn (format t "# ~40,1,1,@<'~a'~> | X~%" nome-teste)
+                  (format t "  = valor esperado: ~a~%" valor-esperado)
+                  (format t "  = valor obtido: ~a~%" valor-real)
+            )
+        )
+  )
 )
 
-(defun verificar-nao-igual (valor-esperado valor-real &key (nome-teste "*TESTE-GENERICO*"))
-    (cond ((not (equal valor-real valor-esperado)) (format t "# ~a | -~%" nome-teste))
-          (t (progn (format t "# ~a | X~%" nome-teste)
-                    (format t " =valor esperado: ~a~%" valor-esperado)
-                    (format t " =valor obtido: ~a~%" valor-real)
-             )
-          )
-    )
+;; Função que verifica se o valor esperado é diferente do valor real
+(defun verificar-nao-igual (valor-esperado valor-real &optional (nome-teste "*TESTE-GENERICO*"))
+  "Verifica se o valor esperado é diferente do valor real"
+  (cond ((not (equal valor-real valor-esperado)) (format t "# ~40,1,1,@<'~a'~> | -~%" nome-teste))
+        (t (progn (format t "# ~40,1,1,@<'~a'~> | X~%" nome-teste)
+                  (format t "  = valor esperado: ~a~%" valor-esperado)
+                  (format t "  = valor obtido: ~a~%" valor-real)
+            )
+        )
+  )
 )
 
 ;;; Testes
 
 ;; Função que carrega todos os testes
-(defun correr-testes ()
-    ;; Carregar ficheiros de código
-    (load (merge-pathnames "src/jogo.lisp" (caminho-raiz)) :verbose nil)
-    (load (merge-pathnames "src/algoritmo.lisp" (caminho-raiz)) :verbose nil)
-    (load (merge-pathnames "src/interact.lisp" (caminho-raiz)) :verbose nil)
+(defun executa-testes ()
+  "Executa todos os testes"
+  ;; Carregar ficheiros de código
+  (load (merge-pathnames "src/jogo.lisp" (caminho-raiz)) :verbose nil)
+  (load (merge-pathnames "src/algoritmo.lisp" (caminho-raiz)) :verbose nil)
+  (load (merge-pathnames "src/interact.lisp" (caminho-raiz)) :verbose nil)
 
-    ;; Carregar ficheiros de teste
-    (load (merge-pathnames "t/jogo-testes.lisp" (caminho-raiz)) :verbose nil)
-    (load (merge-pathnames "t/algoritmo-testes.lisp" (caminho-raiz)) :verbose nil)
-    (load (merge-pathnames "t/interact-testes.lisp" (caminho-raiz)) :verbose nil)
+  ;; Carregar ficheiros de teste
+  (load (merge-pathnames "t/jogo-testes.lisp" (caminho-raiz)) :verbose nil)
+  (load (merge-pathnames "t/algoritmo-testes.lisp" (caminho-raiz)) :verbose nil)
+  (load (merge-pathnames "t/interact-testes.lisp" (caminho-raiz)) :verbose nil)
+)
+
+(defun executa-jogo-testes ()
+  "Executa os testes do jogo"
+  ;; Carregar ficheiros de código
+  (load (merge-pathnames "src/jogo.lisp" (caminho-raiz)) :verbose nil)
+
+  ;; Carregar ficheiros de teste
+  (load (merge-pathnames "t/jogo-testes.lisp" (caminho-raiz)) :verbose nil)
+)
+
+(defun executa-algoritmo-testes ()
+  "Executa os testes do algoritmo"
+  ;; Carregar ficheiros de código
+  (load (merge-pathnames "src/algoritmo.lisp" (caminho-raiz)) :verbose nil)
+
+  ;; Carregar ficheiros de teste
+  (load (merge-pathnames "t/algoritmo-testes.lisp" (caminho-raiz)) :verbose nil)
+)
+
+(defun executa-interact-testes ()
+  "Executa os testes do interact"
+  ;; Carregar ficheiros de código
+  (load (merge-pathnames "src/interact.lisp" (caminho-raiz)) :verbose nil)
+
+  ;; Carregar ficheiros de teste
+  (load (merge-pathnames "t/interact-testes.lisp" (caminho-raiz)) :verbose nil)
 )
 
 ;; Correr todos os testes
