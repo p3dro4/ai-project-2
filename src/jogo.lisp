6405e8e7b0dc05818ee01f2f7858fc41991215a6
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-01-12 11:05:06 +0000

    Add movement functions

--- src/jogo.lisp
+++ src/jogo.lisp
@@ -21,16 +21,16 @@
   )
 )
 
-;; Função que retorna um tabuleiro predefinido com o cavalo colocado.
+;; Função que retorna um tabuleiro predefinido com os cavalos colocados.
 (defun tabuleiro-jogado ()
-  "Tabuleiro de teste igual ao anterior mas tendo sido colocado o cavalo na posição: i=0 e j=0"
+  "Tabuleiro de teste igual ao anterior mas tendo sido colocado o cavalo branco na casa (0,9) e o cavalo preto na casa (9,8)"
   '(
-    (94 25 54 89 21 8 36 14 41 -1)
+    (94 25 54 NIL 21 8 36 14 41 -1) 
     (78 47 56 23 5 49 13 12 26 60) 
-    (0 27 17 83 34 93 74 52 45 80)
-    (69 9 77 95 55 39 91 73 57 30) 
-    (24 15 22 86 1 11 68 79 76 72)
-    (81 48 32 2 64 16 50 37 29 71)
+    (0 27 17 83 34 93 74 52 45 80) 
+    (NIL 9 77 95 55 39 91 73 57 30) 
+    (24 15 22 86 1 11 68 79 76 72) 
+    (81 48 32 2 64 16 50 37 29 71) 
     (99 51 6 18 53 28 7 63 10 88) 
     (59 42 46 85 90 75 87 43 20 31) 
     (3 61 58 44 65 82 19 4 35 62) 
@@ -41,25 +41,25 @@
 ;; Função que recebe um tabuleiro e se não tiver o cavalo (branco ou preto) colocado,
 ;; coloca o cavalo na casa com maior valor na linha respetiva 
 ;; (primeira linha para o cavalo branco e última para o cavalo preto).
-(defun colocar-cavalo (tabuleiro cavalo)
+(defun colocar-cavalo (tabuleiro cavalo &optional (valor-a-remover-se-duplo (maior-numero-tabuleiro tabuleiro)))
   "Coloca o cavalo na casa com maior valor na linha respetiva"
   (cond ((not (numberp cavalo)) (error "O cavalo tem de ser um numero"))
         ((not (or (= cavalo -1) (= cavalo -2))) (error "O cavalo tem de ser -1 ou -2"))
         ((cavalo-colocado-p tabuleiro cavalo) nil)
-        (t (cond ((= cavalo -1) (colocar-cavalo-auxiliar tabuleiro cavalo 0))
-                 ((= cavalo -2) (colocar-cavalo-auxiliar tabuleiro cavalo (1- (length tabuleiro))))
+        (t (cond ((= cavalo -1) (colocar-cavalo-auxiliar tabuleiro cavalo 0 valor-a-remover-se-duplo))
+                 ((= cavalo -2) (colocar-cavalo-auxiliar tabuleiro cavalo (1- (length tabuleiro)) valor-a-remover-se-duplo))
            )
         )
   )
 )
 
 ;; Função auxiliar que recebe um tabuleiro, o cavalo e a linha respetiva e coloca o cavalo na casa com maior valor na linha respetiva.
-(defun colocar-cavalo-auxiliar (tabuleiro cavalo linha-respetiva)
+(defun colocar-cavalo-auxiliar (tabuleiro cavalo linha-respetiva value-a-remover-se-duplo)
   "Coloca o cavalo na casa com maior valor na linha respetiva"
   (let* ((valor (maior-numero-linha (linha linha-respetiva tabuleiro)))
          (coluna (second (posicao-valor tabuleiro valor))))
     (cond ((null (celula linha-respetiva coluna tabuleiro)) nil)
-          (t (aplicar-regras (substituir linha-respetiva coluna tabuleiro cavalo) (celula linha-respetiva coluna tabuleiro)))
+          (t (aplicar-regras (substituir linha-respetiva coluna tabuleiro cavalo) (celula linha-respetiva coluna tabuleiro) value-a-remover-se-duplo))
     )
   )
 )
@@ -140,7 +140,7 @@
 ;; Função que recebe uma linha e retorna o maior número da linha.
 (defun maior-numero-linha (linha-tabuleiro)
   "Retorna o maior número da linha"
-  (apply 'max linha-tabuleiro)
+  (apply 'max (remove-if (lambda (num) (or (null num) (< num 0))) linha-tabuleiro))
 )
 
 ;; Função que recebe um tabuleiro e retorna o maior número do tabuleiro.
@@ -158,7 +158,7 @@
 
 ;; Função que retorna todos os números de um tabuleiro numa lista.
 (defun numeros-tabuleiro (tabuleiro)
-  (remove-if (lambda (num) (or (< num 0) (null num))) (juntar-linhas tabuleiro))
+  (remove-if (lambda (num) (or (null num) (< num 0))) (juntar-linhas tabuleiro))
 )
 
 ;; Função auxiliar que recebe um tabuleiro e retorna uma lista com todas as linhas do tabuleiro juntas.
@@ -175,6 +175,15 @@
   (cond ((null lista) 0)
         ((not (numberp (car lista))) (error "A lista não é composta por números"))
         (t (+ (* (car lista) (expt 10 (1- (length lista)))) (lista-para-numero (cdr lista))))
+  )
+)
+
+;; Função auxiliar que recebe uma lista e um item e retorna se o item está na lista.
+(defun lista-contem (item lista)
+  "Retorna se o item está na lista"
+  (cond ((null lista) nil)
+        ((equal item (car lista)) t)
+        (t (lista-contem item (cdr lista)))
   )
 )
 
@@ -230,7 +239,7 @@
 
 ;; Função que recebe o tabuleiro e devolve a posição (i j) em que se encontra o
 ;; cavalo. Caso o cavalo não se encontre no tabuleiro deverá ser retornado NIL.
-(defun posicao-cavalo (tabuleiro cavalo)
+(defun posicao-cavalo (cavalo tabuleiro)
   "Retorna a posicao do cavalo no tabuleiro"
   (posicao-valor tabuleiro cavalo)
 )
@@ -239,7 +248,7 @@
 ;; verifica se o cavalo está colocado no tabuleiro.
 (defun cavalo-colocado-p (tabuleiro cavalo)
   "Verifica se o cavalo está colocado no tabuleiro"
-  (cond ((posicao-cavalo tabuleiro cavalo) t)
+  (cond ((posicao-cavalo cavalo tabuleiro) t)
         (t nil)
   )
 )
@@ -261,5 +270,76 @@
           )
         )
         (t tabuleiro)
+  )
+)
+
+;;; Movimentos
+
+;; Predicado que recebe o cavalo, a posição de destino e o tabuleiro e
+;; verifica se a posição de destino é válida.
+(defun movimento-valido-p (cavalo destino tabuleiro)
+  "Verifica se a posicao de destino é valida"
+  (cond ((null destino) nil)
+          ((movimento-valido-auxiliar destino tabuleiro) 
+            (not (ameacado-p cavalo destino tabuleiro))
+          )
+          (t nil)
+  )
+)
+
+;; Função auxiliar que recebe a posição de destino e o tabuleiro e
+;; verifica se a posição de destino é válida.
+(defun movimento-valido-auxiliar (destino tabuleiro)
+  "Função auxiliar que verifica se a posicao de destino é valida"
+  (let ((n (length tabuleiro)))
+    (cond ((or (< (first destino) 0) (< (second destino) 0)) nil)
+          ((or (>= (first destino) n) (>= (second destino) n)) nil)
+          ((eq (celula (first destino) (second destino) tabuleiro) nil) nil)
+          (t t)
+    )
+  )
+)
+
+;; Predicado que recebe o cavalo, a posição de destino e o tabuleiro e
+;; verifica se a posição de destino está ameaçada pelo cavalo oposto.
+(defun ameacado-p (cavalo destino tabuleiro)
+  "Verifica se a posicao de destino está ameacada pelo cavalo oposto"
+  (let ((cavalo-oposto (cond ((= cavalo -1) -2) ((= cavalo -2) -1))))
+    (cond ((null destino) nil)
+          ((null (posicao-cavalo cavalo-oposto tabuleiro)) nil)
+          ((null (movimentos-possiveis cavalo-oposto tabuleiro)) nil)
+          ((lista-contem destino (movimentos-possiveis cavalo-oposto tabuleiro)) t)
+    )
+  )
+)
+
+;; Função que recebe o cavalo e o tabuleiro e 
+;;retorna uma lista com todas as posições possíveis para o cavalo.
+(defun movimentos-possiveis (cavalo tabuleiro)
+  "Retorna uma lista com todas as posicoes possiveis para o cavalo"
+  (let ((posicao (posicao-cavalo cavalo tabuleiro)))
+    (cond ((null posicao) nil)
+          (t (let* ((i (first posicao))
+                    (j (second posicao))
+                    (lista-movimentos (list (list (+ i 2) (+ j 1)) (list (+ i 2) (- j 1)) (list (- i 2) (+ j 1)) (list (- i 2) (- j 1))
+                                           (list (+ i 1) (+ j 2)) (list (+ i 1) (- j 2)) (list (- i 1) (+ j 2)) (list (- i 1) (- j 2)))))
+                (remove-if-not (lambda (pos) (movimento-valido-auxiliar pos tabuleiro)) lista-movimentos)
+              )
+          )
+    )
+  )
+)
+
+;; Função auxiliar que recebe o cavalo, a posição de destino e o tabuleiro e
+;; retorna o tabuleiro com o cavalo movido e NIL na casa de onde partiu. Retorna NIL caso a posição de destino não seja válida.
+(defun mover-cavalo (cavalo destino tabuleiro)
+  "Move o cavalo para a posição de destino"
+  (cond ((not (movimento-valido-p cavalo destino tabuleiro)) nil)
+        (t (let* ((posicao (posicao-cavalo cavalo tabuleiro))
+                 (tabuleiro-substituido-posicao (substituir (first posicao) (second posicao) tabuleiro))
+                 (valor-destino (celula (first destino) (second destino) tabuleiro)))
+              (aplicar-regras (substituir (first destino) (second destino) tabuleiro-substituido-posicao cavalo) valor-destino)
+            )
+        )
   )
 )
