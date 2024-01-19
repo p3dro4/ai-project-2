b98b05425df72369348293040b0b1fead4c708f3
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-01-19 16:24:11 +0000

    Add initial implementation of negamax with alpha-beta pruning

--- src/algoritmo.lisp
+++ src/algoritmo.lisp
@@ -11,7 +11,12 @@
   "Cria um nó com o estado, o custo e o nó pai"
   (list estado g pai)
 )
-(export 'cria-no)
+
+;;; Nós
+(defun no-teste ()
+  "Retorna um nó de teste"
+  (cria-no (tabuleiro-ambos-colocados) 2 (cria-no (tabuleiro-cavalo-branco) 1 (cria-no (tabuleiro-teste))))
+)
 
 ;;; Seletores
 
@@ -20,26 +25,39 @@
   "Retorna o estado do nó dado como argumento"
   (first no)
 )
-(export 'no-estado)
 
 ;; Retorna o custo do nó dado como argumento
 (defun no-profundidade (no)
   "Retorna o custo do nó dado como argumento"
   (second no)
 )
-(export 'no-profundidade)
 
 ;; Retorna o nó pai do nó dado como argumento
 (defun no-pai (no)
   "Retorna o nó pai do nó dado como argumento"
   (third no)
 )
-(export 'no-pai)
 
-;;; Algoritmo alfa-beta
+;; Algoritmo negamax com cortes alfa-beta
+(defun negamax-alfabeta (no profundidade alfa beta jogador jogadores funcao-sucessores funcao-utilidade &optional (cache (make-hash-table)))
+  "Algoritmo negamax com cortes alfa-beta"
+  (let ((sucessores (funcall funcao-sucessores no jogador)))
+              (cond ((or (= profundidade 0) (null sucessores)) (cond ((equal jogador (first jogadores)) (funcall funcao-utilidade no jogador)) (t (- (funcall funcao-utilidade no jogador)))))
+                    (t (let* ((melhor-valor (apply 'max (mapcar (lambda (suc) (max (- (negamax-alfabeta suc (1- profundidade) (- beta) (- alfa) (trocar-jogador jogador jogadores) jogadores funcao-sucessores funcao-utilidade cache)))) sucessores)))
+                              (alfa-novo (max alfa melhor-valor)))
+                          (cond ((>= alfa-novo beta) most-negative-double-float)
+                                (t melhor-valor)
+                          )
+                       )
+                    )
+              )
+  )
+)
 
-;; TODO: Função alfa-beta
-
-;; TODO: Função max
-
-;; TODO: Função min
+;; Função que altera o jogador atual.
+(defun trocar-jogador (jogador-atual jogadores)
+  "Função que altera o jogador atual"
+  (cond ((equal jogador-atual (first jogadores)) (second jogadores))
+        ((equal jogador-atual (second jogadores)) (first jogadores))
+  )
+)
